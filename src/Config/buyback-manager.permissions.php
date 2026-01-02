104707e2242ef5e24e634a39ab4231c28f82951c
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2026-01-02 23:52:10 +0000

    Create buyback-manager.permissions.php

---
+++ src/Config/buyback-manager.permissions.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'buyback-manager' => [
+        'view' => [
+            'label' => 'buyback-manager::permissions.view',
+            'description' => 'View buyback contracts and statistics',
+        ],
+        'settings' => [
+            'label' => 'buyback-manager::permissions.settings',
+            'description' => 'Manage buyback settings and pricing rules',
+        ],
+    ],
+];
